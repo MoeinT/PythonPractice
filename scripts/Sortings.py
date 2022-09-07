d1bdab31566e081914c7401545b53d7c5a3302b7
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2022-09-07 22:02:09 +0200

    added another sort algorithms

---
+++ scripts/Sortings.py
@@ -0,0 +1,55 @@
+class MySorting:
+    def __init__(self, arr: list) -> None:
+        self.arr = arr
+
+    def BubbleSort1(self) -> list:
+        l = self.arr
+        for i in range(0, len(l) - 1):
+            for j in range(i + 1, len(l)):
+                if l[i] > l[j]:
+                    l[i], l[j] = l[j], l[i]
+        return l
+
+    def BubbleSort(self) -> list:
+        l = self.arr
+        swap = True
+        while swap:
+            swap = False
+
+            for i in range(0, len(l) - 1):
+                if l[i] > l[i + 1]:
+                    swap = True
+                    l[i], l[i + 1] = l[i + 1], l[i]
+        return l
+
+    def SelectionSort(self) -> list:
+        l = self.arr
+        index = 0
+        swap = True
+        while swap:
+            swap = False
+            for i in range(index, len(l)):
+                if l[i] < l[index]:
+                    swap = True
+                    l[i], l[index] = l[index], l[i]
+            index += 1
+
+        return l
+
+    def InsertionSort(self) -> list:
+        l = self.arr
+        swap = True
+        while swap:
+            swap = False
+            for i in range(1, len(l)):
+                if l[i] < l[i - 1]:
+                    swap = True
+                    l[i], l[i - 1] = l[i - 1], l[i]
+        return l
+
+
+if __name__ == "__main__":
+    l = [1, 9, 0, 2, 7, 5, 8, 3, 0, 3, 11, 5, 2, 8, 4]
+    print(MySorting(l).BubbleSort())
+    print(MySorting(l).SelectionSort())
+    print(MySorting(l).InsertionSort())
